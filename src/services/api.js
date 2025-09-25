2df4d0dc9932ecc4a6dda84c816bbce8caf617b5
thiru2403 <thiruselvam20000@gmail.com>
2025-09-25 14:17:40 +0530

    add the cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: " https://elmer-nonsupposed-debatably.ngrok-free.dev/api" });
+const API = axios.create({
+  baseURL: "https://elmer-nonsupposed-debatably.ngrok-free.dev/api",
+  withCredentials: true,
+});
 
-// Attach token automatically if logged in
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
